ee31e838c9248425ef84f6fad3d4ffac286badd0
King-Windows <krsazon@gmail.com>
2023-06-26 17:50:35 -0700

    Updates EaS

--- script.js
+++ script.js
@@ -2,55 +2,61 @@ const gridContainer = document.querySelector('.grid-container');
 
 const btnCreate = document.querySelector('.btnCreate');
 
-// gridContainer.style['display'] = 'grid';
-// gridContainer.style['grid-template-columns'] = 'repeat(16, 2fr)';
-// gridContainer.style['grid-template-rows'] = 'repeat(16, 2fr)';
-// gridContainer.style['gap'] = '1px';
+gridContainer.style['display'] = 'grid';
+gridContainer.style['grid-template-columns'] = 'repeat(16, 40px)';
+gridContainer.style['grid-template-rows'] = 'repeat(16, 40px)';
+gridContainer.style['gap'] = '1px';
 
 btnCreate.addEventListener('click', () => {
     let gridNumber = prompt('Enter a grid number');
 
+    gridContainer.style['display'] = 'grid';
+    gridContainer.style['grid-template-columns'] = `repeat(${gridNumber}, 40px)`;
+    gridContainer.style['grid-template-rows'] = `repeat(${gridNumber}, 40px)`;
+    gridContainer.style['gap'] = '1px';
+
     console.log(gridNumber * 2);
 
-    for (let i = 0; i < 32; i++) {
+    while (gridContainer.firstChild) {
+        gridContainer.removeChild(gridContainer.firstChild);
+    }
+
+    for (let i = 0; i < gridNumber * gridNumber; i++) {
         const gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
         gridItem.setAttribute('id', i);
         gridContainer.appendChild(gridItem);
-    
+
+
         gridItem.style['background-color'] = "#ccc";
         gridItem.style['border'] = "1px solid black";
 
         gridItem.addEventListener('mouseover', (e) => {
             const hoveredElementId = e.target.id;
-    
+
             console.log(hoveredElementId);
-    
+
             gridItem.style['background-color'] = "black";
         });
     }
-    
+
 });
 
-// gridContainer.style['display'] = 'grid';
-// gridContainer.style['grid-template-columns'] = 'repeat(4, 40px)';
-// gridContainer.style['grid-template-rows'] = 'repeat(4, 40px)';
-// gridContainer.style['gap'] = '1px';
 
-for (let i = 0; i < 256; i++) {
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    gridItem.setAttribute('id', i);
-    gridContainer.appendChild(gridItem);
+// for (let i = 0; i < 0; i++) {
+//     const gridItem = document.createElement('div');
+//     gridItem.classList.add('grid-item');
+//     gridItem.setAttribute('id', i);
+//     gridContainer.appendChild(gridItem);
 
-    gridItem.style['background-color'] = "#ccc";
-    gridItem.style['border'] = "1px solid black";
+//     gridItem.style['background-color'] = "#ccc";
+//     gridItem.style['border'] = "1px solid black";
 
-    gridItem.addEventListener('mouseover', (e) => {
-        const hoveredElementId = e.target.id;
+//     gridItem.addEventListener('mouseover', (e) => {
+//         const hoveredElementId = e.target.id;
 
-        console.log(hoveredElementId);
+//         console.log(hoveredElementId);
 
-        gridItem.style['background-color'] = "black";
-    });
-}
+//         gridItem.style['background-color'] = "black";
+//     });
+// }

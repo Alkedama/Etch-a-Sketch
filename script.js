6417d3615b480c18bb810ca493f7ea40c0d92b07
King-Windows <krsazon@gmail.com>
2023-06-26 17:58:35 -0700

    Updates EaS-UI

--- script.js
+++ script.js
@@ -10,34 +10,37 @@ gridContainer.style['gap'] = '1px';
 btnCreate.addEventListener('click', () => {
     let gridNumber = prompt('Enter a grid number');
 
-    gridContainer.style['display'] = 'grid';
-    gridContainer.style['grid-template-columns'] = `repeat(${gridNumber}, 40px)`;
-    gridContainer.style['grid-template-rows'] = `repeat(${gridNumber}, 40px)`;
-    gridContainer.style['gap'] = '1px';
+    if (gridNumber <= 100 & gridNumber >= 0) {
+        gridContainer.style['display'] = 'grid';
+        gridContainer.style['grid-template-columns'] = `repeat(${gridNumber}, 40px)`;
+        gridContainer.style['grid-template-rows'] = `repeat(${gridNumber}, 40px)`;
+        gridContainer.style['gap'] = '1px';
 
-    console.log(gridNumber * 2);
+        while (gridContainer.firstChild) {
+            gridContainer.removeChild(gridContainer.firstChild);
+        }
 
-    while (gridContainer.firstChild) {
-        gridContainer.removeChild(gridContainer.firstChild);
-    }
-
-    for (let i = 0; i < gridNumber * gridNumber; i++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        gridItem.setAttribute('id', i);
-        gridContainer.appendChild(gridItem);
+        for (let i = 0; i < gridNumber * gridNumber; i++) {
+            const gridItem = document.createElement('div');
+            gridItem.classList.add('grid-item');
+            gridItem.setAttribute('id', i);
+            gridContainer.appendChild(gridItem);
 
 
-        gridItem.style['background-color'] = "#ccc";
-        gridItem.style['border'] = "1px solid black";
+            gridItem.style['background-color'] = "#ccc";
+            gridItem.style['border'] = "1px solid black";
 
-        gridItem.addEventListener('mouseover', (e) => {
-            const hoveredElementId = e.target.id;
+            gridItem.addEventListener('mouseover', (e) => {
+                const hoveredElementId = e.target.id;
 
-            console.log(hoveredElementId);
+                // console.log(hoveredElementId);
 
-            gridItem.style['background-color'] = "black";
-        });
+                gridItem.style['background-color'] = "black";
+            });
+        }
+
+    } else {
+        alert('Invalid input. Grid number must be less than 100');
     }
 
 });
